3c2d859f4425ea0f612d51017e27fcf13640a7d4
antrungkien3470 <kienkaka0312@gmail.com>
2025-04-17 23:23:55 +0700

    Delete BTL ktnc directory

--- BTL ktnc/ALU.v
+++
@@ -1,7 +0,0 @@
-module ALU (
-    input [31:0] a, b,
-    input [1:0] op,
-    output [31:0] result
-);
-    assign result = a + b; // Chỉ hỗ trợ cộng
-endmodule
--- BTL ktnc/ImmediateGenerator.v
+++
@@ -1,7 +0,0 @@
-module ImmediateGenerator (
-    input [31:0] instruction,
-    output [31:0] imm_ext
-);
-    // Chỉ hỗ trợ I-type (addi)
-    assign imm_ext = {{20{instruction[31]}}, instruction[31:20]};
-endmodule
